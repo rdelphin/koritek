3e8bc8d323e099f75686bc8c1fb8310670de0ed4
Kenold Beauplan <something4ken@gmail.com>
2018-10-16 19:07:03 -0400

    comment code block

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -244,10 +244,10 @@ mr = (function (mr, $, window, document){
         
         //////////////// Append .background-image-holder <img>'s as CSS backgrounds
 
-	    $('.background-image-holder').each(function() {
-	        var imgSrc = $(this).children('img').attr('src');
-	        $(this).css('background', 'url("' + imgSrc + '")').css('background-position', 'initial').css('opacity','1');
-	    });
+	    // $('.background-image-holder').each(function() {
+	    //     var imgSrc = $(this).children('img').attr('src');
+	    //     $(this).css('background', 'url("' + imgSrc + '")').css('background-position', 'initial').css('opacity','1');
+	    // });
     };
 
     mr.backgrounds = {
