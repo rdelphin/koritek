06a05867af69d9cc4bceee68487f027c4708d0fc
Roger Delphin <rdelphin@outlook.com>
2023-09-25 23:11:10 -0400

    test

--- tina/config.ts
+++ tina/config.ts
@@ -1,9 +1,4 @@
 import { defineConfig } from "tinacms";
-import { directorsFields } from "./templates";
-import { eventsFields } from "./templates";
-import { pageFields } from "./templates";
-import { postFields } from "./templates";
-import { projectsFields } from "./templates";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
@@ -12,7 +7,7 @@ export default defineConfig({
   branch,
   clientId: "cd73b96d-a2b2-4010-b1e8-946bef75277f", // Get this from tina.io
   token: "3a81e1640a9b3082be7ad1fe8566b300b41bcee6", // Get this from tina.io
-  client: { skip: true },
+
   build: {
     outputFolder: "admin",
     publicFolder: "static",
@@ -26,127 +21,22 @@ export default defineConfig({
   schema: {
     collections: [
       {
-        format: "md",
-        label: "Events",
-        name: "events",
-        path: "content/events",
-        frontmatterFormat: "yaml",
-        match: {
-          include: "**/*",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-          ...eventsFields(),
-        ],
-      },
-      {
-        format: "md",
-        label: "Projects",
-        name: "projects",
-        path: "content/projects",
-        frontmatterFormat: "yaml",
-        match: {
-          include: "**/*",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-          ...projectsFields(),
-        ],
-      },
-      {
-        format: "md",
-        label: "Directors",
-        name: "directors",
-        path: "content/directors",
-        frontmatterFormat: "yaml",
-        match: {
-          include: "**/*",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-          ...directorsFields(),
-        ],
-      },
-      {
-        format: "md",
+        name: "post",
         label: "Posts",
-        name: "posts",
         path: "content/posts",
-        frontmatterFormat: "yaml",
-        match: {
-          include: "**/*",
-        },
         fields: [
           {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-          ...postFields(),
-        ],
-      },
-      {
-        format: "md",
-        label: "Pages",
-        name: "pages",
-        path: "content",
-        frontmatterFormat: "yaml",
-        match: {
-          include: "*",
-        },
-        fields: [
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body of Document",
-            description: "This is the markdown body",
-            isBody: true,
-          },
-          ...pageFields(),
-        ],
-      },
-      {
-        format: "toml",
-        label: "Config",
-        name: "config",
-        path: ".",
-        frontmatterFormat: "yaml",
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-        },
-        match: {
-          include: "config",
-        },
-        fields: [
-          {
-            name: "dummy",
-            label: "Dummy field",
             type: "string",
-            description:
-              "This is a dummy field, please replace it with the fields you want to edit. See https://tina.io/docs/schema/ for more info",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
           },
         ],
       },
